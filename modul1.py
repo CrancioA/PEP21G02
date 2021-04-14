643fc04c135e888237dfbce9554f63e98a3e7a17
Adrian <adrian.cranciova@yahoo.ro>
2021-04-14 21:27:46 +0200

    files added for Module 1

---
+++ modul1.py
@@ -0,0 +1,30 @@
+# print('Hello World')
+# print('print\u200b("")')
+# # print​("")
+# HELLO_WORLD = 'Hello World'
+# print(HELLO_WORLD)
+#
+# # name = 'Adrian'
+# # print(3 * (name + ' '))
+# name = 'Adrian\n'
+# # print(100 * name)
+# print(80 * '#')
+# print(print)
+# print('3')
+
+# print(' ____')
+# print('|   |')
+# print('|', end='')
+# print(3 * '_', end='')
+# print('|')
+
+print('Triunghi: ')
+print("   / \ ")
+print("  /   \ ")
+print(' /     \ ')
+print("/_______\ ")
+print('Patrat: ')
+print(" _____")
+print("|     |")
+print("|_____|")
+
